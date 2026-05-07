d11343bab1235e97c9ed1c698546b3c642605149
MOHAMMED ABDUL BASEER <abdbaseer911@gmail.com>
2026-05-07 13:41:13 +0530

    Initialize App component with routing and layout

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import MarketTicker from './components/MarketTicker';
+import Dashboard from './pages/Dashboard';
+import Screener from './pages/Screener';
+import Portfolio from './pages/Portfolio';
+import Learn from './pages/Learn';
+
+export default function App() {
+  return (
+    <div className="app-layout">
+      <Sidebar />
+      <div style={{ flex: 1, marginLeft: 220 }}>
+        <MarketTicker />
+        <div className="main-content" style={{ marginLeft: 0 }}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/screener" element={<Screener />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/learn" element={<Learn />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
